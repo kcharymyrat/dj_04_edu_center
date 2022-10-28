3d717f9e5aae58c4c7aaa063136e8fd4cead192d
Charymyrat Garryyev <kcharymyrat@gmail.com>
2022-10-29 02:41:17 +0500

    Created Category and Course models

--- center/models.py
+++ center/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Course(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    rayat_price = models.DecimalField(max_digits=8, decimal_places=2)
+    student_price = models.DecimalField(max_digits=8, decimal_places=2)
+    discount_amount = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
